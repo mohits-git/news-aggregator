298b94d5586dbff8dbb6f4f0d5bd7454687cd2d6
mohits-git <mohitkhatri2805@gmail.com>
2025-10-27 23:11:32 +0530

    init: project setup with primeng, custom primeng theme preset and eslint setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
+import { customPreset } from './shared/theme/custom-preset';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: customPreset,
+        options: {
+          darkModeSelector: false || 'none',
+        }
+      },
+    }),
+  ],
 };
