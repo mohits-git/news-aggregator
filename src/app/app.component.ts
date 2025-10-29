0f9cf05570fe00902a6a891aefbbed3094708c18
mohits-git <mohitkhatri2805@gmail.com>
2025-10-29 05:43:20 +0000

    refac: move layout to shared/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LayoutComponent } from './layout/layout.component';
+import { LayoutComponent } from '@/shared/layout/layout.component';
 import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, LayoutComponent, ToastModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-}
+export class AppComponent {}
